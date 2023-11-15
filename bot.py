bdc9624f62c2e12e4b114f864396be28e71f8124
XCV0 <frt.mrartem@gmail.com>
2023-11-15 23:12:41 +0300

    я выгорел

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
-# марк если ты это читаешь нам пиздаs
+import asyncio
+from aiogram import Bot, Dispatcher
+
+TOKEN = ""
+
+
+async def main():
+    bot = Bot(token=TOKEN)
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
